bb327e0005b9c7629f9f8fe3897312a7dac74548
Divya Bilolikar <66176164+dliviya@users.noreply.github.com>
2022-02-26 15:48:26 -0800

    Delete project_n4w2b-master directory

--- project_n4w2b-master/src/main/persistence/Writable.java
+++
@@ -1,9 +0,0 @@
-package persistence;
-
-import org.json.JSONObject;
-
-//Modelled on JSON code provided by instructors
-public interface Writable {
-    // EFFECTS: returns this as JSON object
-    JSONObject toJson();
-}
